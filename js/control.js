ba958960f1ea1892160d94031d0e23b6d584c291
MeGoogle <manonhorizon@gmail.com>
2024-01-13 02:17:24 +0300

    Let UP and DOWN be pressed simultaneously

--- js/control.js
+++ js/control.js
@@ -1,17 +1,32 @@
-function keyPressed() {
-  if (keyCode === UP_ARROW) {
-    mainCharacter.verticalShift(-1);
+const keysPressed = new Set();
+
+const calcVerticalShift = () => {
+  if (
+    keysPressed.has(UP_ARROW) && keysPressed.has(DOWN_ARROW) ||
+    !keysPressed.has(UP_ARROW) && !keysPressed.has(DOWN_ARROW)
+  ) {
+    return 0;
   }
-  if (keyCode === DOWN_ARROW) {
-    mainCharacter.verticalShift(1);
+  if (keysPressed.has(UP_ARROW)) {
+    return -1;
+  }
+  return 1;
+}
+
+function keyPressed() {
+  if ((keyCode === UP_ARROW) || (keyCode === DOWN_ARROW)) {
+    if (!keysPressed.has(keyCode)) {
+      keysPressed.add(keyCode);
+    }
+
+    mainCharacter.verticalShift(calcVerticalShift());
   }
 }
 
 function keyReleased() {
-  if (keyCode === UP_ARROW) {
-    mainCharacter.verticalShift(0);
-  }
-  if (keyCode === DOWN_ARROW) {
-    mainCharacter.verticalShift(0);
+
+  if ((keyCode === UP_ARROW) || (keyCode === DOWN_ARROW)) {
+    keysPressed.delete(keyCode);
+    mainCharacter.verticalShift(calcVerticalShift());
   }
 }
